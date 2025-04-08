bf3a9e5bfb8593d3ec050a56f6f0eb4815cd2812
marjorieblanco18 <marjorieblanco978@gmail.com>
2025-04-08 16:40:17 +0800

    activity 4

---
+++ about.php
@@ -0,0 +1,17 @@
+    <!--About Section-->
+    <section id="about">
+      <div class="container">
+          <div class="row align-items-center">
+              <div class="col-lg-6">
+                <h3>Lorem</h3>
+                <h2>Lorem ipsum dolor sit amet.</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde voluptatem ullam, error a nulla sed, vel excepturi vitae repellendus non tempore soluta quia impedit rem fugit quisquam repudiandae dignissimos dolore!</p>
+
+                <a href="#" class= "btn btn-click"> click</a>
+              </div>
+              <div class="col-lg-6">
+                <img src="syy.jpg" alt="">
+               </div>
+             </div>
+          </div>
+                  </section>
